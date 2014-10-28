d6ec1f81e6d8e8ba0c416c5386a6a3b613e71e59
Rafael Genovez Idalgo <rafael.genovez@mpro.mp.br>
2014-10-28 14:13:57 -0700

    Retirando testes desnecessários de sessão.

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -14,26 +14,10 @@ public class SessaoTest {
 	}
 	
 	@Test
-	public void deveVender5ingressosSeHa10vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(10);
-		
-		Assert.assertTrue(sessao.podeReservar(5));
-	}
-	
-	@Test
 	public void deveVender1IngressoSeHa1Ingresso() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(1);
 		Assert.assertTrue(sessao.podeReservar(1));
-	}
-
-	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
-
-		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
